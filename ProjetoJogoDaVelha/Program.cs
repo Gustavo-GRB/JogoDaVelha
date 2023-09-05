06bccf8d1e5cda228594f2037708c59a95dabfe0
gustavo.rocha <gustavorocha.net@hotmail.com>
2023-09-05 18:51:12 -0300

    Implementado lógica de Alternância de Jogadores e Exibição do Tabuleiro

--- ProjetoJogoDaVelha/Program.cs
+++ ProjetoJogoDaVelha/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Numerics;
 using System;
+using System.Security.Cryptography;
 
 Console.WriteLine("       ____   ___    ____   ___       ___     ____      __ __    ___  _      __ __   ____ ");
 Console.WriteLine("      |    | /   \\  /    | /   \\     |   \\   /    |    |  |  |  /  _]| |    |  |  | /    |");
@@ -47,12 +48,12 @@ if (string.Equals(sorteio, "S", StringComparison.OrdinalIgnoreCase))
 
     if (numeroAleatorio == 1)
     {
-        Console.WriteLine($"Jogador {jogardor1} você começa a primeira jogada");
+        Console.WriteLine($"Jogador(a) {jogardor1} você começa a primeira jogada");
     }
     else
     {
-        Console.WriteLine($"Jogador {jogardor2} você começa a primeira jogada");
-    }    
+        Console.WriteLine($"Jogador(a) {jogardor2} você começa a primeira jogada");
+    }
 }
 
 else if (string.Equals(sorteio, "N", StringComparison.OrdinalIgnoreCase))
@@ -62,12 +63,7 @@ else if (string.Equals(sorteio, "N", StringComparison.OrdinalIgnoreCase))
 }
 
 
-//se o numeroAleatorio == 1 jogardor1 comeca senão jogador2 comeca
-//se o jogador digita numero 1, == linha 1 coluna1...
-
-
-//int[,] array = new int[3, 3] { {1, 2 ,3},{4, 5, 6}, {7, 8, 9 } } ; //substituir para string
-string[,] array = new string[3, 3] { { "1", "2" ,"3"},{"4", "5", "6"}, {"7", "8", "9" } };
+string[,] array = new string[3, 3] { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
 
 Console.WriteLine("                |     |     ");
 Console.WriteLine("              1 |  2  |  3  ");
@@ -82,107 +78,181 @@ Console.WriteLine("              7 |  8  |  9  ");
 Console.WriteLine("                |     |     ");
 
 
-
-
-
-
-//Console.WriteLine("Agora é a vez do jogador: ",jogador);
-
-string  posicao;
+string posicao;
 bool vezDoJogador1 = true;
 
-Console.WriteLine("{jogardor1}Digite o número da posição: ");
-posicao = Console.ReadLine();
-//if (posicao = 1)
-if (int.Parse(posicao) == 1)
+for (int i = 0; i < 9; i++)
 {
-    if (vezDoJogador1 = true)
+    Console.Clear();
+    tabuleiro(array);
+
+    if (vezDoJogador1)
     {
-        array[0, 0] = "X";
+        Console.WriteLine($"\n-> {jogardor1} Digite o número da posição conforme os números acima: ");
+        posicao = Console.ReadLine();
+        Console.Clear();
     }
     else
-        array[0, 0] = "O";
-}
-else if (int.Parse(posicao) == 2)
-{
-    if (vezDoJogador1 = true)
     {
-        array[0, 1] = "X";
+        Console.WriteLine($"\n-> {jogardor2} Digite o número da posição conforme os números acima: ");
+        posicao = Console.ReadLine();
+        Console.Clear();
     }
-    else
-        array[0, 1] = "O";
-}
-else if (int.Parse(posicao) == 3)
-{
-    if (vezDoJogador1 = true)
+        
+    
+
+    if (int.Parse(posicao) == 1)
     {
-        array[0, 2] = "X";
+        if (vezDoJogador1)
+        {
+            array[0, 0] = "X";
+            vezDoJogador1 = false;
+        }
+        else
+        {
+            array[0, 0] = "O";
+            vezDoJogador1 = true;
+        }
     }
-    else
-        array[0, 2] = "O";
-}
-else if (int.Parse(posicao) == 4)
-{
-    if (vezDoJogador1 = true)
+    else if (int.Parse(posicao) == 2)
     {
-        array[1, 0] = "X";
+        if (vezDoJogador1)
+        {
+            array[0, 1] = "X";
+            vezDoJogador1 = false;
+        }
+        else
+        {
+            array[0, 1] = "O";
+            vezDoJogador1 = true;
+        }
     }
-    else
-        array[1, 0] = "O";
-}
-else if (int.Parse(posicao) == 5)
-{
-    if (vezDoJogador1 = true)
+    else if (int.Parse(posicao) == 3)
     {
-        array[1, 1] = "X";
+        if (vezDoJogador1)
+        {
+            array[0, 2] = "X";
+            vezDoJogador1 = false;
+        }
+        else
+        {
+            array[0, 2] = "O";
+            vezDoJogador1 = true;
+        }
+
     }
-    else
-        array[1, 1] = "O";
-}
-else if (int.Parse(posicao) == 6)
-{
-    if (vezDoJogador1 = true)
+    else if (int.Parse(posicao) == 4)
     {
-        array[1, 2] = "X";
+        if (vezDoJogador1)
+        {
+            array[1, 0] = "X";
+            vezDoJogador1 = false;
+        }
+        else
+        {
+            array[1, 0] = "O";
+            vezDoJogador1 = true;
+        }
     }
-    else
-        array[1, 2] = "O";
-}
-else if (int.Parse(posicao) == 7)
-{
-    if (vezDoJogador1 = true)
+    else if (int.Parse(posicao) == 5)
     {
-        array[2, 0] = "X";
+        if (vezDoJogador1)
+        {
+            array[1, 1] = "X";
+            vezDoJogador1 = false;
+        }
+        else
+        {
+            array[1, 1] = "O";
+            vezDoJogador1 = true;
+        }
     }
-    else
-        array[2, 0] = "O";
-}
-else if (int.Parse(posicao) == 8)
-{
-    if (vezDoJogador1 = true)
+    else if (int.Parse(posicao) == 6)
     {
-        array[2, 1] = "X";
+        if (vezDoJogador1)
+        {
+            array[1, 2] = "X";
+            vezDoJogador1 = false;
+        }
+        else
+        {
+            array[1, 2] = "O";
+            vezDoJogador1 = true;
+        }
     }
-    else
-        array[2, 1] = "O";
-}
-else if (int.Parse(posicao) == 9)
-{
-    if (vezDoJogador1 = true)
+    else if (int.Parse(posicao) == 7)
     {
-        array[2, 2] = "X";
+        if (vezDoJogador1 = true)
+        {
+            array[2, 0] = "X";
+            vezDoJogador1 = false;
+        }
+        else
+        {
+            array[2, 0] = "O";
+            vezDoJogador1 = true;
+        }
     }
-    else
-        array[2, 2] = "O";
+    else if (int.Parse(posicao) == 8)
+    {
+        if (vezDoJogador1 = true)
+        {
+            array[2, 1] = "X";
+            vezDoJogador1 = false;
+        }
+        else
+        {
+            array[2, 1] = "O";
+            vezDoJogador1 = true;
+        }
+    }
+    else if (int.Parse(posicao) == 9)
+    {
+        if (vezDoJogador1 = true)
+        {
+            array[2, 2] = "X";
+            vezDoJogador1 = false;
+        }
+        else
+        {
+            array[2, 2] = "O";
+            vezDoJogador1 = true;
+        }
+
+    }
 }
 
-//testando
-Console.Write(array[0, 0]);
-Console.Write(array[0, 1]);
-Console.WriteLine(array[0, 2]);
-Console.Write(array[1, 0]);
-Console.Write(array[1, 1]);
-Console.WriteLine(array[1, 2]);
-Console.Write(array[2, 0]);
-Console.Write(array[2, 1]);
-Console.Write(array[2, 2]);
+
+
+
+
+static void tabuleiro(string[,] array)
+{
+    string p1 = array[0, 0];
+    string p2 = array[0, 1];
+    string p3 = array[0, 2];
+    string p4 = array[1, 0];
+    string p5 = array[1, 1];
+    string p6 = array[1, 2];
+    string p7 = array[2, 0];
+    string p8 = array[2, 1];
+    string p9 = array[2, 2];
+
+    Console.WriteLine("                |     |     ");
+    Console.WriteLine($"              {p1} |  {p2}  |  {p3}  ");
+    Console.WriteLine("                |     |     ");
+    Console.WriteLine("            ----------------");
+    Console.WriteLine("                |     |     ");
+    Console.WriteLine($"              {p4} |  {p5}  |  {p6}  ");
+    Console.WriteLine("                |     |     ");
+    Console.WriteLine("            ----------------");
+    Console.WriteLine("                |     |     ");
+    Console.WriteLine($"              {p7} |  {p8}  |  {p9}  ");
+    Console.WriteLine("                |     |     ");
+
+}
+
+
+
+
+
