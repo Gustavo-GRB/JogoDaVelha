0d51cb31c4844b5351b11a2ffcf6ee6b512a1dca
gustavo.rocha <gustavorocha.net@hotmail.com>
2023-09-08 18:01:26 -0300

    Correções e implantado regra para jogador 1 não escolher uma posição já preenchida ou não existente

--- ProjetoJogoDaVelha/Program.cs
+++ ProjetoJogoDaVelha/Program.cs
@@ -35,8 +35,9 @@ int numeroAleatorio;
 
 do
 {
-    Console.WriteLine("Deseja sortear quem deve começar? 'S' Sim / 'N' Não ");
+    Console.WriteLine("Deseja sortear quem deve começar? 'S' Sim / 'N' Não");
     sorteio = Console.ReadLine();
+    Console.WriteLine();
 }
 while (!string.Equals(sorteio, "S", StringComparison.OrdinalIgnoreCase) && !string.Equals(sorteio, "N", StringComparison.OrdinalIgnoreCase));
 
@@ -63,44 +64,109 @@ else if (string.Equals(sorteio, "N", StringComparison.OrdinalIgnoreCase))
 }
 
 
-string[,] array = new string[3, 3] { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+string[,] array = new string[3, 3] { 
+    { "1", "2", "3" }, 
+    { "4", "5", "6" }, 
+    { "7", "8", "9" } };
 
-Console.WriteLine("                |     |     ");
-Console.WriteLine("              1 |  2  |  3  ");
-Console.WriteLine("                |     |     ");
-Console.WriteLine("            ----------------");
-Console.WriteLine("                |     |     ");
-Console.WriteLine("              4 |  5  |  6  ");
-Console.WriteLine("                |     |     ");
-Console.WriteLine("            ----------------");
-Console.WriteLine("                |     |     ");
-Console.WriteLine("              7 |  8  |  9  ");
-Console.WriteLine("                |     |     ");
-
-
-string posicao;
 bool vezDoJogador1 = true;
+string posicao = "0";
 
 for (int i = 0; i < 9; i++)
 {
-    Console.Clear();
-    tabuleiro(array);
+    bool jogadaInvalida = false;
+    exibirTabuleiro(array);
 
-    if (vezDoJogador1)
-    {
-        Console.WriteLine($"\n-> {jogardor1} Digite o número da posição conforme os números acima: ");
-        posicao = Console.ReadLine();
-        Console.Clear();
-    }
-    else
-    {
-        Console.WriteLine($"\n-> {jogardor2} Digite o número da posição conforme os números acima: ");
-        posicao = Console.ReadLine();
-        Console.Clear();
-    }
-        
-    
+    if (!jogadaInvalida)
+    {        
+        if (vezDoJogador1)
+        {
+            Console.WriteLine($"\n-> {jogardor1} Digite o número da posição conforme os números acima: ");
+            posicao = Console.ReadLine();
+            Console.Clear();
 
+            bool numeroEncontrado = false;
+
+            foreach (var item in array)
+            {
+                if (item == posicao)
+                {
+                    numeroEncontrado = true;
+                    break;
+                }
+            }
+            if (!numeroEncontrado)
+            {
+
+                while (!numeroEncontrado)
+                {
+                    Console.Clear();
+                    exibirTabuleiro (array);
+                    jogadaInvalida = true;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("***** Posição escolhida não existe ou já foi escolhida! *****");
+                    Console.ResetColor();
+                    
+                    Console.WriteLine($"\n-> {jogardor1} Digite o número da posição conforme os números acima: ");
+                    posicao = Console.ReadLine();
+
+                    //duplicado
+                    foreach (var item in array)
+                    {
+                        if (item == posicao)
+                        {
+                            numeroEncontrado = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine($"\n-> {jogardor2} Digite o número da posição conforme os números acima: ");
+            posicao = Console.ReadLine();
+            Console.Clear();
+        }
+    }
+
+    vezDoJogador1 = PreencherTabuleiro(array, posicao, vezDoJogador1);
+
+
+
+}
+
+
+
+
+
+static void exibirTabuleiro(string[,] array)
+{
+    string p1 = array[0, 0];
+    string p2 = array[0, 1];
+    string p3 = array[0, 2];
+    string p4 = array[1, 0];
+    string p5 = array[1, 1];
+    string p6 = array[1, 2];
+    string p7 = array[2, 0];
+    string p8 = array[2, 1];
+    string p9 = array[2, 2];
+
+    Console.WriteLine("                |     |     ");
+    Console.WriteLine($"              {p1} |  {p2}  |  {p3}  ");
+    Console.WriteLine("                |     |     ");
+    Console.WriteLine("            ----------------");
+    Console.WriteLine("                |     |     ");
+    Console.WriteLine($"              {p4} |  {p5}  |  {p6}  ");
+    Console.WriteLine("                |     |     ");
+    Console.WriteLine("            ----------------");
+    Console.WriteLine("                |     |     ");
+    Console.WriteLine($"              {p7} |  {p8}  |  {p9}  ");
+    Console.WriteLine("                |     |     ");
+}
+
+static bool PreencherTabuleiro(string[,] array, string posicao, bool vezDoJogador1)
+{
     if (int.Parse(posicao) == 1)
     {
         if (vezDoJogador1)
@@ -182,7 +248,7 @@ for (int i = 0; i < 9; i++)
     }
     else if (int.Parse(posicao) == 7)
     {
-        if (vezDoJogador1 = true)
+        if (vezDoJogador1)
         {
             array[2, 0] = "X";
             vezDoJogador1 = false;
@@ -195,7 +261,7 @@ for (int i = 0; i < 9; i++)
     }
     else if (int.Parse(posicao) == 8)
     {
-        if (vezDoJogador1 = true)
+        if (vezDoJogador1)
         {
             array[2, 1] = "X";
             vezDoJogador1 = false;
@@ -208,7 +274,7 @@ for (int i = 0; i < 9; i++)
     }
     else if (int.Parse(posicao) == 9)
     {
-        if (vezDoJogador1 = true)
+        if (vezDoJogador1)
         {
             array[2, 2] = "X";
             vezDoJogador1 = false;
@@ -220,39 +286,13 @@ for (int i = 0; i < 9; i++)
         }
 
     }
+
+    return vezDoJogador1;
 }
 
 
-
-
-
-static void tabuleiro(string[,] array)
-{
-    string p1 = array[0, 0];
-    string p2 = array[0, 1];
-    string p3 = array[0, 2];
-    string p4 = array[1, 0];
-    string p5 = array[1, 1];
-    string p6 = array[1, 2];
-    string p7 = array[2, 0];
-    string p8 = array[2, 1];
-    string p9 = array[2, 2];
-
-    Console.WriteLine("                |     |     ");
-    Console.WriteLine($"              {p1} |  {p2}  |  {p3}  ");
-    Console.WriteLine("                |     |     ");
-    Console.WriteLine("            ----------------");
-    Console.WriteLine("                |     |     ");
-    Console.WriteLine($"              {p4} |  {p5}  |  {p6}  ");
-    Console.WriteLine("                |     |     ");
-    Console.WriteLine("            ----------------");
-    Console.WriteLine("                |     |     ");
-    Console.WriteLine($"              {p7} |  {p8}  |  {p9}  ");
-    Console.WriteLine("                |     |     ");
-
-}
-
-
-
-
-
+/*
+ * -Corrigido a mensagem de quem deve começar primeiro
+ * -Iniciando a implantação da regra de posicao ocupada e posição não existente
+ * testar escolhendo numeros diferentes na primeira jogada(aparentemente o num 1 funfa agora testar resto
+ */
